a4e6ede6e63cfed9e90063cd1fe8f22a75396f47
David Moreno <morenopd@campusemav.com>
2020-10-05 11:03:04 +0200

    Classe 5/10/2020
    Fins al controlador del player

---
+++ Assets/Scripts/SCRPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCRPlayer : MonoBehaviour
+{
+    [SerializeField]
+    float Velocitat = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y; //per llegir el gamepad
+        x = Input.GetAxis("Horizontal"); //llegim moviment horitzontal  
+        y = Input.GetAxis("Vertical"); //llegim moviment vertical
+
+        //print("Horizontal: " + x + "/ Vertical " + y); //mostrarem x y 
+        transform.Translate(x *Time.deltaTime * Velocitat , y *Time.deltaTime *Velocitat, 0);
+    }
+}
